b7690bca156766891f6d35549bee2e7ad3de9f2a
Icelycoool <mohammedcali@proton.me>
2024-10-02 01:09:19 +0300

    13. Iterating through report objects done

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,9 @@
+export default function createIteratorObject(report) {
+  let allEmployees = [];
+  for (const department in report.allEmployees) {
+    if (Object.hasOwnProperty.call(report.allEmployees, department)) {
+      allEmployees = allEmployees.concat(report.allEmployees[department]);
+    }
+  }
+  return allEmployees[Symbol.iterator]();
+}
