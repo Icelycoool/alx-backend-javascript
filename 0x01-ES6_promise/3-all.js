00b99cb4d66272c0340e4a627c651e8c3befaa2f
Icelycoool <mohammedcali@proton.me>
2024-10-03 01:57:48 +0300

    3. Handle multiple successful promises done

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,16 @@
+import { uploadPhoto, createUser } from './utils';
+
+const promises = [uploadPhoto(), createUser()];
+
+export default function handleProfileSignup() {
+  Promise.all(promises)
+    .then((results) => {
+      const profile = results[0].body;
+      const { firstName } = results[1];
+      const { lastName } = results[1];
+      console.log(`${profile} ${firstName} ${lastName}`);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
+}
