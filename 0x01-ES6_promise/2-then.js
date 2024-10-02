5aacdcfd97658e119dd2753e7a65dbb05cc765c9
Icelycoool <mohammedcali@proton.me>
2024-10-03 01:07:13 +0300

    2. Catch me if you can! done

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -7,8 +7,5 @@ export default function handleResponseFromAPI(promise) {
     .catch(() => {
       console.log('Got a response from the API');
       return new Error();
-    })
-    .finally(() => {
-      console.log('Got a response from the API');
     });
 }
