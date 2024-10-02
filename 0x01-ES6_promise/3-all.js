d6bc6aefb650cf4206409ad284cd63f70e1b703f
Icelycoool <mohammedcali@proton.me>
2024-10-03 02:09:21 +0300

    3. Handle multiple successful promises done

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,13 +1,10 @@
 import { uploadPhoto, createUser } from './utils';
 
-const promises = [uploadPhoto(), createUser()];
-
 export default function handleProfileSignup() {
-  return Promise.all(promises)
+  return Promise.all([uploadPhoto(), createUser()])
     .then((results) => {
       const profile = results[0].body;
-      const { firstName } = results[1];
-      const { lastName } = results[1];
+      const { firstName, lastName } = results[1];
       console.log(`${profile} ${firstName} ${lastName}`);
     })
     .catch(() => {
