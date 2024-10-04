dbdf1d88ff6a8d94f889af70cee86a8d02f2d66a
Icelycoool <mohammedcali@proton.me>
2024-10-04 22:21:32 +0300

    5. A Building updated

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,18 +1,15 @@
 export default class Building {
   constructor(sqft) {
-    this._sqft = sqft;
-    if (new.target !== Building) {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
+    if (this.constructor !== Building) {
+      if (typeof this.evacuationWarningMessage !== 'function') {
+        throw new Error('Class extending Building must override evacuationWarningMessage');
+      }
     }
+    this._sqft = sqft;
   }
 
   get sqft() {
     return this._sqft;
   }
 
-  evacuationWarningMessage() {
-    if (this.evacuationWarningMessage !== 'function') {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
-    }
-  }
 }
