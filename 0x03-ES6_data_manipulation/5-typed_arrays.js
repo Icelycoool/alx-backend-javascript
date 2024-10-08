dcf7ddb36b82aa68fb905ee7342095cae4db1e9d
Icelycoool <mohammedcali@proton.me>
2024-10-09 02:24:59 +0300

    5. Typed Arrays output corrected

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,12 +1,13 @@
 export default function createInt8TypedArray(length, position, value) {
   const array = new ArrayBuffer(length);
-  const int8Array = new Int8Array(array);
 
-  if (position < 0 || position > length) {
+  if (position < 0 || position >= length) {
     throw new Error('Position outside range');
   }
 
-  int8Array[position] = value;
+  const dataView = new DataView(array);
 
-  return array;
+  dataView.setInt8(position, value);
+
+  return dataView;
 }
