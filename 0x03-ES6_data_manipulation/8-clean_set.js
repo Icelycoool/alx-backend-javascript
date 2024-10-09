75307ee9eceb697f59a5732c11fa00424e24bb7d
Icelycoool <mohammedcali@proton.me>
2024-10-09 18:27:41 +0300

    8. Clean set

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,12 @@
+const cleanSet = (set, startString) => {
+  const result = [];
+  set.forEach((value) => {
+    if (value.startsWith(startString)) {
+      result.push(value.slice(startString.length));
+    }
+  });
+
+  return result.join('-');
+};
+
+export default cleanSet;
