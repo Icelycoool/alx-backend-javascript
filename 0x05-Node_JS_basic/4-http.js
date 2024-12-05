171d4fb95f83571cf6b467ce470025c9ed148300
Icelycoool <mohammedcali@proton.me>
2024-12-05 18:10:14 +0300

    4. Create a small HTTP server using Node's HTTP module final

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -5,16 +5,16 @@ const HOST = 'localhost';
 const app = http.createServer();
 
 app.on('request', (_, res) => {
-  const text = 'Hello ALX!';
+  const responseText = 'Hello Holberton School!';
 
   res.setHeader('Content-Type', 'text/plain');
-  res.setHeader('Content-Length', text.length);
+  res.setHeader('Content-Length', responseText.length);
   res.statusCode = 200;
-  res.write(Buffer.from(text));
+  res.write(Buffer.from(responseText));
 });
 
 app.listen(PORT, HOST, () => {
-  process.stdout.write(`...`);
+  process.stdout.write(`Server listening at -> http://${HOST}:${PORT}\n`);
 });
 
 module.exports = app;
