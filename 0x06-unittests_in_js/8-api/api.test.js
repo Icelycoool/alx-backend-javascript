ae7a569fed5218164dab9f9641d4630844384ed4
Icelycoool <mohammedcali@proton.me>
2024-12-09 16:29:22 +0300

    8. Basic Integration testing

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,38 +1,14 @@
-const { expect } = require('chai');
 const request = require('request');
+const { expect } = require('chai');
 
-describe('Index page', () => {
-  let server;
+describe('API integration test', () => {
+  const API_URL = 'http://localhost:7865';
 
-  before((done) => {
-    server = require('./api');
-    done();
-  });
-
-  after((done) => {
-    server.close();
-    done();
-  });
-
-  it('should return correct status code', (done) => {
-    request.get('http://localhost:7865', (err, res, body) => {
-      expect(res.statusCode).to.equal(200);
-      done();
-    });
-  });
-
-  it('should return the correct result', (done) => {
-    request.get('http://localhost:7865', (err, res, body) => {
-      expect(body).to.equal('Welcome to the payment system');
-      done();
-    });
-  });
-
-  it('should have the correct headers', (done) => {
-    request.get('http://localhost:7865', (err, res, body) => {
-      expect(res.headers['content-type']).to.include('text/html; charset=utf-8');
+  it('GET / returns correct response', (done) => {
+    request.get(`${API_URL}/`, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Welcome to the payment system');
       done();
     });
   });
 });
-
